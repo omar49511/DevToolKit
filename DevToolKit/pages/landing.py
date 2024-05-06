9e413e5b05ad904e6fee119015b3e93991702337
omar49511 <omar.reyesz18@tectijuana.edu.mx>
2024-05-05 22:23:09 -0700

    navbar created

--- DevToolKit/pages/landing.py
+++ DevToolKit/pages/landing.py
@@ -3,12 +3,13 @@ from DevToolKit.components.hero import hero
 from DevToolKit.components.feature import feature
 from DevToolKit.components.overview import overview
 from DevToolKit.components.TagStream import TagStream
-
+from DevToolKit.components.navbar.navbar import navbar
 @rx.page(route="/")
 def landing():
     return rx.vstack(
-            hero(),
-            overview(),
-            feature(),
-            TagStream()
+        navbar(sidebar=""),
+        hero(),
+        overview(),
+        feature(),
+        TagStream()
     ),
